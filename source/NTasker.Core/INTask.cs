e726fcef55c6507344b7056019437821b1e643fc
Eyassu Getachew <eyassug@gmail.com>
2016-06-06 14:39:56 +0300

    Defined base interface INTask

---
+++ source/NTasker.Core/INTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTasker.Core
+{
+    /// <summary>
+    /// Base interface for INTasker that should be exposed by components
+    /// </summary>
+    public interface INTask
+    {
+        Task Execute();
+    }
+}
