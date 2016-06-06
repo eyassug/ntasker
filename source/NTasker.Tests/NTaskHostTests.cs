d4f76d69b7ec2bcdf85622c80a22fb61a5312ce0
Eyassu Getachew <eyassug@gmail.com>
2016-06-06 17:48:38 +0300

    Changed directory paths to be relative

--- source/NTasker.Tests/NTaskHostTests.cs
+++ source/NTasker.Tests/NTaskHostTests.cs
@@ -12,7 +12,7 @@ namespace NTasker.Tests
         public void NTaskHost_InitializationTests()
         {
             var hostName = "DefaultHost";
-            var sampleDirectory = @"C:\Users\Eyassu\Documents\Projects\Personal Open Source\NTasker\source\NTasker.Core.Tests\bin\Debug";
+            var sampleDirectory = @"..\..\..\NTasker.Core.Tests\bin\Debug";
             var host = new NTasker.NTaskHost(hostName, sampleDirectory);
             Assert.IsNotNull(host.Name);
             Assert.AreEqual(hostName, host.Name);
@@ -24,7 +24,7 @@ namespace NTasker.Tests
         public async Task NTaskHost_ContainerShouldNotBeNullAfterInit()
         {
             var hostName = "DefaultHost";
-            var sampleDirectory = @"C:\Users\Eyassu\Documents\Projects\Personal Open Source\NTasker\source\NTasker.Core.Tests\bin\Debug";
+            var sampleDirectory = @"..\..\..\NTasker.Core.Tests\bin\Debug";
             var host = new NTasker.NTaskHost(hostName, sampleDirectory);
             var token = new CancellationToken(false);
             await host.Init(new CancellationToken(false));
