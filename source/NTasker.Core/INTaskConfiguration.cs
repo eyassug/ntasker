2658b0439168b45fcf70be8a69ac711916187bb3
Eyassu Getachew <eyassug@gmail.com>
2016-06-06 15:29:24 +0300

    Added INTaskConfiguration interface to define execution frequency

---
+++ source/NTasker.Core/INTaskConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTasker.Core
+{
+    /// <summary>
+    /// Base interface for configuring INTask implementations
+    /// </summary>
+    public interface INTaskConfiguration
+    {
+        /// <summary>
+        /// Task frequency in milliseconds
+        /// </summary>
+        long Frequency { get; }
+        
+    }
+}
