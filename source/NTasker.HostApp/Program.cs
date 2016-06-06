3eba1b89ed9e09ba9b991c81d7a6295438b14dbc
Eyassu Getachew <eyassug@gmail.com>
2016-06-06 17:57:42 +0300

    Added a host application that discovers tasks from assemblies residing in a directory and executes them

---
+++ source/NTasker.HostApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NTasker.HostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            var hostName = "DefaultHost";
+            // TODO: Get this from args parameter
+            var sampleDirectory = @"..\..\..\NTasker.Core.Tests\bin\Debug";
+            var host = new NTasker.NTaskHost(hostName, sampleDirectory);
+            // Create a token that never expires
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            // Create a token that expires after N seconds
+            // var tokenSource = new CancellationTokenSource(40 * 1000);
+            // var token = tokenSource.Token;
+            host.Init(token).Wait();            
+            Console.Read();
+        }
+    }
+}
